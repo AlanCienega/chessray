5a9946d71e70abfd81f62fa47a07ed38970f0d7b
AlanCienega <craulalan115@gmail.com>
2023-10-04 23:55:23 -0600

    draw king and pawn shadows

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 board = document.getElementById("board");
+buttons = document.querySelectorAll(".btn");
+body = document.querySelector("body");
 
 // variables
 height = window.innerHeight;
@@ -36,9 +38,25 @@ setSize(black_bishop);
 setSize(black_knight);
 setSize(black_pawn);
 
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    changeTheme(event.target.classList[1]);
+  });
+});
+
+function changeTheme(theme) {
+  console.log(theme);
+  body.style.background = theme.split("-")[1];
+}
+
+function dropShadows(piece_name) {
+  let shadows = document.querySelectorAll(`.${piece_name}_class`);
+  shadows.forEach((shadow) => shadow.remove());
+}
 function addDraggingClass(element) {
   element.addEventListener("dragstart", () => {
     element.classList.add("dragging");
+    dropShadows(element.id);
   });
   element.addEventListener("dragend", () => {
     element.classList.remove("dragging");
@@ -59,15 +77,29 @@ addDraggingClass(black_bishop);
 addDraggingClass(black_knight);
 addDraggingClass(black_pawn);
 
+function fillSquare(element, x, y) {
+  if ((x + y) % 2 != 0) {
+    element.style.background = "#a0a0a0";
+  } else {
+    element.style.background = "#e0e0e0";
+  }
+}
+
 function drawSquare(x, y) {
   square = document.createElement("div");
   square.classList.add("square");
   square.id = `${x},${y}`;
   setSize(square);
-  if ((x + y) % 2 != 0) {
-    square.style.background = "gray";
-  }
+  fillSquare(square, x, y);
   board.append(square);
+}
+function drawShadow(color, piece_name) {
+  shadow = document.createElement("div");
+  shadow.classList.add(`${piece_name}_class`);
+  shadow.style.background = getTransparentColorPiece(color);
+  shadow.style.position = "absolute";
+  setSize(shadow);
+  return shadow;
 }
 
 function setSize(element) {
@@ -79,12 +111,46 @@ function setSize(element) {
 function drawBoard() {
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-      drawSquare(i, j);
+      drawSquare(j, i);
     }
   }
 }
 
 drawBoard();
+
+function getTransparentColorPiece(color) {
+  let rgba_color = `rgba( ${color == "black" ? "255" : "0"}, 0, ${
+    color == "white" ? "255" : "0"
+  }, 0.3)`;
+  return rgba_color;
+}
+
+function handleKingNeighboringCells(x, y, piece) {
+  const color = piece.id.split("_")[0];
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (i == 0 && j == 0) continue;
+      let cell = document.getElementById(`${x + i},${y + j}`);
+      if (cell) {
+        cell.append(drawShadow(color, piece.id));
+      }
+    }
+  }
+}
+
+function handlePawnNeighboringCells(x, y, piece) {
+  const color = piece.id.split("_")[0];
+  let offsetY = color == "white" ? -1 : 1;
+  let left_cell = document.getElementById(`${x - 1},${y + offsetY}`);
+  let right_cell = document.getElementById(`${x + 1},${y + offsetY}`);
+
+  if (left_cell) {
+    left_cell.append(drawShadow(color, piece.id));
+  }
+  if (right_cell) {
+    right_cell.append(drawShadow(color, piece.id));
+  }
+}
 
 squares = document.querySelectorAll(".square");
 squares.forEach((square) => {
@@ -92,19 +158,28 @@ squares.forEach((square) => {
     e.preventDefault();
     const current_piece = document.querySelector(".dragging");
     current_piece.style.display = "inline-block";
+
     const color_piece = current_piece.id.split("_")[0];
+    // keep color piece
     current_piece.style.color = color_piece;
 
-    current_piece.style.background = `rgba( ${
-      color_piece == "black" ? "255" : "0"
-    }, 0, ${color_piece == "white" ? "255" : "0"}, 0.3)`;
+    const rgba = getTransparentColorPiece(color_piece);
 
-    let coords = e.target.id.split(",");
-    let x, y;
-    if (coords.length > 1) {
-      x = parseInt(coords[0]);
-      y = parseInt(coords[1]);
-    }
+    current_piece.style.background = rgba;
+
     square.append(current_piece);
+  });
+  square.addEventListener("drop", (e) => {
+    const piece = e.target;
+    const piece_name = e.target.id.split("_")[1];
+    let coords = e.originalTarget.parentElement.id.split(",");
+    let x = parseInt(coords[0]);
+    let y = parseInt(coords[1]);
+
+    if (piece_name == "king") {
+      handleKingNeighboringCells(x, y, piece);
+    } else if (piece_name == "pawn") {
+      handlePawnNeighboringCells(x, y, piece);
+    }
   });
 });
